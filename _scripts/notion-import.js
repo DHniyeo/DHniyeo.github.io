43902e7e4e730bc25f1fada9ae16cadef9daa9d5
DHniyeo <64246124+DHniyeo@users.noreply.github.com>
2024-07-07 17:22:02 +0900

    Update notion-import.js

--- _scripts/notion-import.js
+++ _scripts/notion-import.js
@@ -1,24 +1,24 @@
-const { Client } = require("@notionhq/client");
-const { NotionToMarkdown } = require("notion-to-md");
-const fs = require("fs");
-const moment = require("moment");
-const path = require("path");
-const axios = require("axios");
+const { Client } = require("@notionhq/client"); // Notion API 클라이언트.
+const { NotionToMarkdown } = require("notion-to-md"); // Notion 페이지를 Markdown 형식으로 변환하기 위한 라이브러리
+const fs = require("fs"); // 파일 시스템 관련 모듈
+const moment = require("moment"); // 날짜와 시간을 다루기 위한 모듈
+const path = require("path"); // 파일 경로 관련 모듈
+const axios = require("axios"); // HTTP 요청을 보내기 위한 모듈
+// or
+// import {NotionToMarkdown} from "notion-to-md";
 
-const notion = new Client({
+const notion = new Client({ // Github 환경 변수에서 Notion API 토큰을 가져와 클라이언트를 초기화합니다. Node.js 환경변수 호출은 process.env
   auth: process.env.NOTION_TOKEN,
 });
 
-// Escape code blocks with Jekyll raw tags
-function escapeCodeBlock(body) {
+function escapeCodeBlock(body) { // Markdown 코드 블록을 Jekyll에서 사용 가능한 {% raw %} 태그로 감싸는 함수입니다.
   const regex = /```([\s\S]*?)```/g;
-  return body.replace(regex, (match, htmlBlock) => {
-    return `\n{% raw %}\n\`\`\`${htmlBlock.trim()}\n\`\`\`\n{% endraw %}\n`;
+  return body.replace(regex, function (match, htmlBlock) {
+    return "\n{% raw %}\n```" + htmlBlock.trim() + "\n```\n{% endraw %}\n";
   });
 }
 
-// Replace H1 titles outside raw blocks with H2
-function replaceTitleOutsideRawBlocks(body) {
+function replaceTitleOutsideRawBlocks(body) { // 제목 레벨을 변경하는 함수입니다. 블록 내부의 내용은 건드리지 않습니다.
   const rawBlocks = [];
   const placeholder = "%%RAW_BLOCK%%";
   body = body.replace(/{% raw %}[\s\S]*?{% endraw %}/g, (match) => {
@@ -26,8 +26,9 @@ function replaceTitleOutsideRawBlocks(body) {
     return placeholder;
   });
 
-  body = body.replace(/\n#[^\n]+\n/g, (match) => {
-    return `\n${match.replace("\n#", "\n##")}`;
+  const regex = /\n#[^\n]+\n/g;
+  body = body.replace(regex, function (match) {
+    return "\n" + match.replace("\n#", "\n##");
   });
 
   rawBlocks.forEach(block => {
@@ -37,13 +38,15 @@ function replaceTitleOutsideRawBlocks(body) {
   return body;
 }
 
-// Create directory if it doesn't exist
-function ensureDirectoryExists(directory) {
-  fs.mkdirSync(directory, { recursive: true });
-}
+// passing notion client to the option
+const n2m = new NotionToMarkdown({ notionClient: notion });
 
-// Fetch Notion pages with filter
-async function fetchNotionPages(databaseId) {
+(async () => {
+  // ensure directory exists
+  const root = "_posts";
+  fs.mkdirSync(root, { recursive: true }); // recursive 를 true 로 두면 내부 디렉토리 자동 생성해줌.
+
+  const databaseId = process.env.DATABASE_ID;
   let response = await notion.databases.query({
     database_id: databaseId,
     filter: {
@@ -55,10 +58,11 @@ async function fetchNotionPages(databaseId) {
   });
 
   const pages = response.results;
-  while (response.has_more) {
+  while (response.has_more) { // 다음 페이지가 남아 있는지 확인하여 pages에 추가
+    const nextCursor = response.next_cursor;
     response = await notion.databases.query({
       database_id: databaseId,
-      start_cursor: response.next_cursor,
+      start_cursor: nextCursor,
       filter: {
         property: "공개",
         checkbox: {
@@ -68,101 +72,110 @@ async function fetchNotionPages(databaseId) {
     });
     pages.push(...response.results);
   }
-  return pages;
-}
 
-// Extract page properties
-function extractPageProperties(page) {
-  const id = page.id;
-  let date = moment(page.created_time).format("YYYY-MM-DD");
-  const pdate = page.properties?.["날짜"]?.["date"]?.["start"];
-  if (pdate) {
-    date = moment(pdate).format("YYYY-MM-DD");
-  }
+  for (const r of pages) { // 페이지 단위 처리
+    const id = r.id;
+    // date
+    let date = moment(r.created_time).format("YYYY-MM-DD");
+    let pdate = r.properties?.["날짜"]?.["date"]?.["start"];
+    if (pdate) {
+      date = moment(pdate).format("YYYY-MM-DD");
+    }
+    // title
+    let title = id;
+    let ptitle = r.properties?.["게시물"]?.["title"];
+    if (ptitle?.length > 0) {
+      title = ptitle[0]?.["plain_text"];
+    }
+    // tags
+    let tags = [];
+    let ptags = r.properties?.["태그"]?.["multi_select"];
+    for (const t of ptags) {
+      const n = t?.["name"];
+      if (n) {
+        tags.push(n);
+      }
+    }
+    // categories
+    let cats = [];
+    let pcats = r.properties?.["카테고리"]?.["multi_select"];
+    for (const t of pcats) {
+      const n = t?.["name"];
+      if (n) {
+        cats.push(n);
+      }
+    }
 
-  let title = id;
-  const ptitle = page.properties?.["게시물"]?.["title"];
-  if (ptitle?.length > 0) {
-    title = ptitle[0]?.["plain_text"];
-  }
-
-  const tags = page.properties?.["태그"]?.["multi_select"]?.map(t => t?.["name"]).filter(Boolean) || [];
-  const categories = page.properties?.["카테고리"]?.["multi_select"]?.map(t => t?.["name"]).filter(Boolean) || [];
-
-  return { id, date, title, tags, categories };
-}
-
-// Generate frontmatter
-function generateFrontmatter(date, title, tags, categories) {
-  const fmtags = tags.length > 0 ? `\ntags: [${tags.join(", ")}]` : "";
-  const fmcats = categories.length > 0 ? `\ncategories: [${categories.join(", ")}]` : "";
-
-  return `---
+    // frontmatter
+    let fmtags = "";
+    let fmcats = "";
+    if (tags.length > 0) {
+      fmtags += "\ntags: [";
+      for (const t of tags) {
+        fmtags += t + ", ";
+      }
+      fmtags += "]";
+    }
+    if (cats.length > 0) {
+      fmcats += "\ncategories: [";
+      for (const t of cats) {
+        fmcats += t + ", ";
+      }
+      fmcats += "]";
+    }
+    const fm = `---
 layout: post
 date: ${date}
 title: "${title}"${fmtags}${fmcats}
 ---
+
 `;
-}
-
-// Download images and replace image links in Markdown
-async function downloadAndReplaceImages(md, ftitle) {
-  let index = 0;
-  return md.replace(/!\[(.*?)\]\((.*?)\)/g, (match, p1, p2) => {
-    const dirname = path.join("assets/img", ftitle);
-    if (!fs.existsSync(dirname)) {
-      fs.mkdirSync(dirname, { recursive: true });
-    }
-    const filename = path.join(dirname, `${index}.png`);
-
-    axios.get(p2, { responseType: "stream" })
-      .then(response => {
-        const file = fs.createWriteStream(filename);
-        response.data.pipe(file);
-      })
-      .catch(console.error);
-
-    const altText = p1 ? `_${p1}_` : "";
-    return `![${index++}](/${filename})${altText}`;
-  });
-}
-
-// Write content to file
-function writeToFile(directory, filename, content) {
-  fs.writeFile(path.join(directory, filename), content, (err) => {
-    if (err) {
-      console.error(err);
-    }
-  });
-}
-
-(async () => {
-  // Ensure root directory exists
-  const root = "_posts";
-  ensureDirectoryExists(root);
-
-  // Fetch pages from Notion
-  const databaseId = process.env.DATABASE_ID;
-  const pages = await fetchNotionPages(databaseId);
-
-  // Process each page
-  for (const page of pages) {
-    const { id, date, title, tags, categories } = extractPageProperties(page);
-    const frontmatter = generateFrontmatter(date, title, tags, categories);
-
-    // Convert Notion page to Markdown
-    const n2m = new NotionToMarkdown({ notionClient: notion });
     const mdblocks = await n2m.pageToMarkdown(id);
     let md = n2m.toMarkdownString(mdblocks)["parent"];
-    if (!md) continue;
-
-    // Process Markdown content
+    if (md === "") {
+      continue;
+    }
     md = escapeCodeBlock(md);
     md = replaceTitleOutsideRawBlocks(md);
-    const ftitle = `${date}-${title.replaceAll(" ", "-")}.md`;
-    md = await downloadAndReplaceImages(md, ftitle);
 
-    // Write Markdown file
-    writeToFile(root, ftitle, frontmatter + md);
+    const ftitle = `${date}-${title.replaceAll(" ", "-")}.md`;
+
+    let index = 0;
+    let edited_md = md.replace(
+      /!\[(.*?)\]\((.*?)\)/g,
+      function (match, p1, p2, p3) {
+        const dirname = path.join("assets/img", ftitle);
+        if (!fs.existsSync(dirname)) {
+          fs.mkdirSync(dirname, { recursive: true });
+        }
+        const filename = path.join(dirname, `${index}.png`);
+
+        axios({
+          method: "get",
+          url: p2,
+          responseType: "stream",
+        })
+          .then(function (response) {
+            let file = fs.createWriteStream(`${filename}`);
+            response.data.pipe(file);
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+
+        let res;
+        if (p1 === "") res = "";
+        else res = `_${p1}_`;
+
+        return `![${index++}](/${filename})${res}`;
+      }
+    );
+
+    //writing to file
+    fs.writeFile(path.join(root, ftitle), fm + edited_md, (err) => {
+      if (err) {
+        console.log(err);
+      }
+    });
   }
 })();
